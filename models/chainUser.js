48b485cdd2487da23110e8bbfc52808ffe9ba1d9
strawhat <uputpal16@ggmail.com>
2019-08-22 23:11:25 +0530

    new models added

--- models/chainUser.js
+++ models/chainUser.js
@@ -1,34 +1,36 @@
 const mongoose = require("mongoose"),
-  Schema = mongoose.Schema,
-  bcrypt = require("bcrypt"),
-  SALT_WORK_FACTOR = 10;
+	Schema = mongoose.Schema,
+	bcrypt = require("bcrypt"),
+	SALT_WORK_FACTOR = 10;
 
 let chainSchema = new Schema({
-  password: { type: String, required: true },
-  name: { type: String, required: true },
-  pubKey: { type: String, required: true },
-  privKey: { type: String, required: true },
-  mnemoninc: { type: String, required: true },
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+	name: { type: String, required: true },
+	pubKey: { type: String, required: true },
+	privKey: { type: String, required: true },
+	mnemoninc: { type: String, required: true },
+	datesaved: { type: Number, required: true }
 });
 
 chainSchema.pre("save", function (next) {
-  var registration = this;
-  if (!registration.isModified("password")) return next();
-  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(registration.password, salt, function (err, hash) {
-      if (err) return next(err);
-      registration.password = hash;
-      next();
-    });
-  });
+	var registration = this;
+	if (!registration.isModified("password")) return next();
+	bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash(registration.password, salt, function (err, hash) {
+			if (err) return next(err);
+			registration.password = hash;
+			next();
+		});
+	});
 });
 
 chainSchema.methods.comparePassword = function (candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
+	bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
 };
 
 module.exports = mongoose.model("Chain", chainSchema);
--- routes/chainUsers.js
+++ routes/chainUsers.js
@@ -4,144 +4,96 @@ var router = express.Router();
 var utils = require('./utils');
 // Normal registration takes three parameters: name, emailId, password
 router.post("/register", async (req, res) => {
-  var newUser = new JobSeeker();
-  newUser.name = req.body.name;
-  newUser.password = req.body.password;
-  var a = await utils.generate();
-  console.log(a);
-  newUser.mnemoninc = a.mnemoninc;
-  newUser.pubKey = a.publicKey;
-  newUser.privKey = a.masterPrivateKey;
-  // res.send({"messagge": a});
-  console.log(newUser);
-  newUser.save(function (err, user) {
-    if (err) {
-      console.log(err);
-      if (err.toString().slice(12, 18) == "E11000") {
-        res.send({ message: "User already registered", emailId: req.body.emailId });
-        return;
-      }
-      res.send({ message: "error while registration" });
-      return;
-    } else {
-      res.status(201).send({ message: "User created", userId: user._id });
-    }
-  });
+	var newUser = new JobSeeker();
+	newUser.username = req.body.username;
+	newUser.name = req.body.name;
+	newUser.password = req.body.password;
+	var a = await utils.generate();
+	console.log(a);
+	newUser.mnemoninc = a.mnemoninc;
+	newUser.pubKey = a.publicKey;
+	newUser.privKey = a.masterPrivateKey;
+	newUser.datesaved = (new Date()).getTime();
+	// res.send({"messagge": a});
+	console.log(newUser);
+	newUser.save(function (err, user) {
+		if (err) {
+			console.log(err);
+			if (err.toString().slice(12, 18) == "E11000") {
+				res.send({ message: "User already registered", username: req.body.username });
+				return;
+			}
+			res.send({ message: "error while registration" });
+			return;
+		} else {
+			res.status(201).send({ message: "User created", userId: req.body.username });
+		}
+	});
 });
 
 // Login for username(emailId) password
 router.post('/login', (req, res) => {
-  console.log(req.body);
-  JobSeeker.findOne({ _id: req.body.id }, function (err, user) {
-    if (err) {
-      res.json({ message: "User not found" });
-      return;
-    }
-    else if (!user) {
-      res.json({ message: "User not found" });
-      return;
-    }
-    user.comparePassword(req.body.password, function (err, isMatch) {
-      if (err) {
-        res.status(405).send({
-          authenticated: false,
-          message: "Internal server error"
-        });
-        return;
-      }
-      if (isMatch)
-        res.status(200).send({ authenticated: isMatch, message: "Logged in successfully" });
-      else
-        res.status(401).send({ authenticated: isMatch, message: "Username or password is incorrect" });
-    });
-  });
+	console.log(req.body);
+	JobSeeker.findOne({ username: req.body.username }, function (err, user) {
+		if (err) {
+			res.json({ message: "User not found" });
+			return;
+		}
+		else if (!user) {
+			res.json({ message: "User not found" });
+			return;
+		}
+		user.comparePassword(req.body.password, function (err, isMatch) {
+			if (err) {
+				res.status(405).send({
+					authenticated: false,
+					message: "Internal server error"
+				});
+				return;
+			}
+			if (isMatch)
+				res.status(200).send({ authenticated: isMatch, message: "Logged in successfully" });
+			else
+				res.status(401).send({ authenticated: isMatch, message: "Username or password is incorrect" });
+		});
+	});
 });
 
 // get a user by id
-router.get('/user/:id', (req, res) => {
-  JobSeeker.findOne({ username: req.params.emailId }, function (err, user) {
-    if (err) {
-      res.json({ message: "User not found" });
-      return;
-    }
-    res.status(200).send({ message: "User found", data: user });
+router.get('/user/:usernmae', (req, res) => {
+	JobSeeker.findOne({ username: req.params.username }, function (err, user) {
+		if (err) {
+			res.json({ message: "User not found" });
+			return;
+		}
+		res.status(200).send({ message: "User found", data: user });
 
-  });
+	});
 });
 
 
-// add personal details to the user
-router.put("/updatepersonaldetails/:emailId", (req, res) => {
-  console.log(req.body);
+router.get('/dashboarddata', (req, res) => {
+	let data = {}
+	JobSeeker.find().sort({ datesaved: 1 }).limit(50).then((users) => {
+		data.users = users;
+		JobSeeker.count().then((count) => {
+			data.count = count;
+			JobSeeker.find({
+				"datesaved":
+				{
+					$gte: (new Date()).getTime() + 86400000,
+					$lt: (new Date()).getTime()
+				}
+			}).then((list) => {
+				data.lastDay = list.length
+			})
+		});
+		res.send(data);
+		return;
+	});
+	res.send("You are wrong");
 
-  JobSeeker.findOneAndUpdate({
-    emailId: req.params.emailId
-  }, {
-      $set: {
-        profilePicture: req.body.profilePicture,
-        DOB: req.body.DOB,
-        nationality: req.body.nationality,
-        currentLocation: req.body.currentLocation,
-        phoneNo: req.body.phoneNo,
-        gender: "F",
-        coverLetter: req.body.coverLetter,
-        videoCv: req.body.videoCv,
-        resume: req.body.resume
-      }
-    }, {
-      upsert: true
-    }, function (err, ) {
-      if (err) {
-        res.send('error updating registration details');
-        return;
-      }
-      res.status(201).send({message: "Personal Details updated successfully", data: req.body });
-    });
-});
-
-
-router.put("/addexperience/:emailId", (req, res) => {
-  JobSeeker.findOneAndUpdate({
-    emailId: req.params.emailId
-  }, {
-      $push: {
-        workExperiences: req.body.experienceList
-      }
-    }, {
-      upsert: true
-    }, function (err, ) {
-      if (err) {
-        res.send('error updating registration details');
-        return;
-      }
-      res.status(201).send({
-        message: "Experiencce details updated",
-        data: req.body.experienceList
-      });
-    });
-});
-//
-
-// router.put("/addexperience/:emailId", (req, res) => {
-//   JobSeeker.findOneAndUpdate({
-//     emailId: req.params.emailId
-//   }, {
-//       $push: {
-//         workExperiences: req.body.experienceList
-//       }
-//     }, {
-//       upsert: true
-//     }, function (err, ) {
-//       if (err) {
-//         res.send('error updating registration details');
-//         return;
-//       }
-//       res.status(201).send({
-//         message: "Experiencce details updated",
-//         data: req.body.experienceList
-//       });
-//     });
-// });
+})
 
 
 module.exports = router;
