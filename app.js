ebc273675f772490b40987d647d797117e566f95
strawhat <uputpal16@ggmail.com>
2019-09-07 17:54:15 +0530

    ssl

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cors = require('cors');
 var port = 8081;
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 mongoose.connect("mongodb://localhost:27017/chain", {
     useNewUrlParser: true,
     useCreateIndex: true
 });
+
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 
 // mongoose.connect(db);
 // app.options('*', cors());
@@ -31,6 +46,13 @@ app.get("/", function (req, res) {
     res.send("tes express nodejs mongodb");
 });
 
-app.listen(port, '0.0.0.0', function () {
-    console.log("app listening on port: " + port);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
